5abf8e792a31b8197b952b27d80c8f153a8021d3
Sydrah Al-saegh <sydrah@Sydrahs-MacBook-Pro.local>
2018-07-23 13:46:59 -0700

    first commit

---
+++ Recycle Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recycle Game
+//
+//  Created by Sydrah Al-saegh on 7/23/18.
+//  Copyright © 2018 Sydrah Al-saegh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
